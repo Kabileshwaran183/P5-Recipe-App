58bd25b4bbf3433ff5f778714bea96fa32cb5914
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2024-12-16 11:03:02 +0530

    Header section created(with styling)
    The header section of our project-Recipe app is well created.

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header(){
+    return (
+        <header className="header">
+            <img src="/assets/logo"></img>
+            <h1>Recipe App</h1>
+        </header>
+            
+        
+    )
+}
