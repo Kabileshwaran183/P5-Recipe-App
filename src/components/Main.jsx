0f6298f8540cf3e09751807b7bbbd735622d6747
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2024-12-16 11:19:04 +0530

    Ingredients form is created

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,14 @@
 export default function Main(){
     return(
         <>
-        
+        <main>
+            <form className="add-ingredient-form">
+                <input type="text"
+                        placeholder="enter ingredient"
+                        aria-label="Enter Recipe"></input>
+                <button>Add Ingredient</button>
+            </form>
+        </main>
         
         </>
     )
