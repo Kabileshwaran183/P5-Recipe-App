109ab506ab0a1670634eea0505163a7da481dd26
Kabileshwaran183 <Kabileshwaran183@gmail.com>
2024-12-17 09:57:57 +0530

    handling the submit event (add ingredients)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,40 @@
+import React from 'react'
 export default function Main(){
+    // const ingredients = ["chicken","vegnoodles","briyani"]
+    const [ingredients,setIngredients] = React.useState([])
+    const ingredientListItems =ingredients.map(i=>(
+        <li key={i}>{i}</li>
+    ))
+
+    function handleSubmit(event){
+        event.preventDefault()
+        const formEl = event.currentTarget
+        const formData = new FormData(formEl)
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients=>[...prevIngredients,newIngredient])
+        formEl.reset()
+    }
+
+/*----action = {handleSubmit} its not working
+    function handleSubmit(formData){
+        const newIngredient = formData.get("ingredient")
+        setIngredients(prevIngredients=>[...prevIngredients,newIngredient])
+        formEl.reset()
+    }
+*/
     return(
         <>
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit} className="add-ingredient-form">
                 <input type="text"
                         placeholder="enter ingredient"
-                        aria-label="Enter Recipe"></input>
-                <button>Add Ingredient</button>
+                        aria-label="Enter Recipe"
+                        name="ingredient" />
+                <button >Add Ingredient</button>
             </form>
+            <ul>
+                {ingredientListItems}
+            </ul>
         </main>
         
         </>
